dbc25272904b9d1193a9b078183da68bf3ee13de
Benjamin Ruiz-Tagle Valderrama <benjamin@Benjamins-iMac.local>
2017-08-30 16:29:19 -0300

    createshape v2

--- createshape.py
+++ createshape.py
@@ -1,11 +1,19 @@
 import os
 import sys
 import getopt
+import timeit
+import shutil
+
+
+
 
 #Plugins path for qgis
 #OSX: /Applications/QGIS.app/Contents/Resources/python
 #Linux: /usr/share/qgis/python/plugins
 sys.path.append("/Applications/QGIS.app/Contents/Resources/python")
+
+#plugins path for processing (QGIS)
+sys.path.append("/Applications/QGIS.app/Contents/Resources/python/plugins")
  
 from qgis.core import *
 from datetime import datetime
@@ -13,12 +21,13 @@ from datetime import datetime
 def usage():
     print "\n Create ShapeFile from VIIRS data"
     print " -d: directory for save ShapeFile"
+    print " -m: directory of masks shapes folder (Chilean Zones)"
 
 def initConfig():
     ligand = ""
     protein = ""
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'd:h', ['directory=', 'help'])
+        opts, args = getopt.getopt(sys.argv[1:], 'd:m:h', ['directory=', 'maskdir=','help'])
         if not opts:
           print ' Missing parameters'
           usage()
@@ -28,17 +37,24 @@ def initConfig():
         sys.exit(2)
 
     for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            usage()
-            sys.exit(2)
-        elif opt in ('-d', '--directory'):
-            directory = arg
-        else:
-            usage()
-            sys.exit(2)
-    return directory
+		if opt in ('-h', '--help'):
+		    usage()
+		    sys.exit(2)
+		elif opt in ('-d', '--directory'):
+		    directory = arg
+		elif opt in ('-m', '--maskdir'):
+		    maskdirectory = arg
+		else:
+		    usage()
+		    sys.exit(2)
+    return [directory, maskdirectory]
+
 
 if __name__ == "__main__":
+
+	directories = initConfig()
+	directory = directories[0]
+	maskDirectory = directories[1]
 
 	app = QgsApplication([],False)
 
@@ -49,26 +65,32 @@ if __name__ == "__main__":
 
 	app.initQgis()
 
-	directory = initConfig()
+	import processing
+	from processing.core.Processing import Processing
+
+	Processing.initialize()
 
 	#Get filename for the day
 	day_of_year = datetime.now().timetuple().tm_yday
 	year = datetime.now().year
 	dayfolder = str(year)+str(day_of_year)
 
-	if not os.path.isdir(directory+"/"+dayfolder):
+	dayfolderPath = os.path.join(directory, dayfolder)
+
+	if not os.path.isdir(dayfolderPath):
 		print "Directory not found, no viirs data for processing"
 	else:
 		#Set name and path of the cvs file (for input and output)
-		logPath = directory+"/"+dayfolder+"/VIIRS_log.log"
+		logPath =  os.path.join(dayfolderPath, "VIIRS_log.log")
 		if not os.path.isfile(logPath):
 			print "Log not found."
 		else:
 			logFile = open(logPath)
 			timeupdate = logFile.readlines()[-1].strip()
+			logFile.close()
 
-			viirsname = directory+dayfolder+"/VIIRS_I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate+"_clean.txt"
-			shapefile = directory+dayfolder+"/VIIRS__I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate
+			viirsname = os.path.join(dayfolderPath, "VIIRS_I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate+".txt")
+			shapefile = os.path.join(dayfolderPath, "VIIRS__I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate+".shp")
 
 			#Set uri for make the qgis vector layer
 			uri = 'file:///%s?crs=%s&xField=%s&yField=%s&decimal=%s&spatialIndex=%s&subsetIndex=%s&watchFile=%s&encoding=%s&geomType=%s&delimiter=%s' % (viirsname, 'EPSG:4326', 'longitude', 'latitude', '.', 'no', 'no', 'no', 'UTF-8', 'point',',')
@@ -79,11 +101,93 @@ if __name__ == "__main__":
 				print "Layer error"
 			else:		
 				#Save as shapefile
-				QgsVectorFileWriter.writeAsVectorFormat(layer, shapefile + '.shp', 'UTF-8', layer.crs(), 'ESRI Shapefile')
-				print "Shapefile complete"
+				QgsVectorFileWriter.writeAsVectorFormat(layer, shapefile, 'UTF-8', layer.crs(), 'ESRI Shapefile')
+				#print "Shapefile complete"
 
+				tempPath = os.path.join(dayfolderPath, "temp")
 
+				if not os.path.isdir(tempPath):
+					os.makedirs(tempPath)
 
+				start_time = timeit.default_timer()
+				temp01Path = os.path.join(tempPath, "temp_nortegrande.shp")
+				temp02Path = os.path.join(tempPath, "temp_nortechico.shp")
+				temp03Path = os.path.join(tempPath, "temp_centro.shp")
+				temp04Path = os.path.join(tempPath, "temp_sur.shp")
+				temp05Path = os.path.join(tempPath, "temp_austral.shp")
+
+				norteGPath = os.path.join(maskDirectory, "MASCARAS_FOCOS_POR_ZONAS", "01_ZONA_NORTE_GRANDE.shp")
+				norteCPath = os.path.join(maskDirectory, "MASCARAS_FOCOS_POR_ZONAS", "02_ZONA_NORTE_CHICO.shp")
+				centroPath = os.path.join(maskDirectory, "MASCARAS_FOCOS_POR_ZONAS", "03_ZONA_CENTRAL.shp")
+				surPath = os.path.join(maskDirectory, "MASCARAS_FOCOS_POR_ZONAS", "04_ZONA_SUR.shp") 
+				australPath = os.path.join(maskDirectory, "MASCARAS_FOCOS_POR_ZONAS", "05_ZONA_AUSTRAL.shp")
+
+				viirs_layer = QgsVectorLayer(shapefile, "partial_layer", "ogr")
+				#viirs_layer = QgsVectorLayer(os.path.join(dayfolderPath, "viirstest.shp"), "partial_layer", "ogr")
+
+				northG_layer =	QgsVectorLayer(norteGPath, "norteG_vector_Layer", "ogr")
+				northC_layer =	QgsVectorLayer(norteCPath, "norteCh_vector_Layer", "ogr")
+				center_layer =	QgsVectorLayer(centroPath, "central_vector_Layer", "ogr")
+				south_layer =	QgsVectorLayer(surPath, "south_vector_Layer", "ogr")
+				austral_layer = QgsVectorLayer(australPath, "austral_vector_Layer", "ogr")
+				#print "Layer upload complete"
+
+				clipLayerPath = ""
+
+				processing.runalg("qgis:clip", viirs_layer, northG_layer, temp01Path)
+				tempNorthG_layer = QgsVectorLayer(temp01Path, "northg", "ogr")
+				if not tempNorthG_layer.isValid():
+					print "North Gr clip layer error"
+				else:
+					clipLayerPath += temp01Path+";"
+
+				processing.runalg("qgis:clip", viirs_layer, northC_layer, temp02Path)
+				tempNorthC_layer = QgsVectorLayer(temp02Path, "northc", "ogr")
+				if not tempNorthC_layer.isValid():
+					print "North Ch clip layer error"
+				else:
+					clipLayerPath += temp02Path+";"
+
+				processing.runalg("qgis:clip", viirs_layer, center_layer, temp03Path)
+				tempCenter_layer = QgsVectorLayer(temp03Path, "center", "ogr")
+				if not tempCenter_layer.isValid():
+					print "Center clip layer error"
+				else:
+					clipLayerPath += temp03Path+";"
+
+				processing.runalg("qgis:clip", viirs_layer, south_layer, temp04Path)
+				tempSouth_layer = QgsVectorLayer(temp04Path, "south", "ogr")
+				if not tempSouth_layer.isValid():
+					print "South clip layer error"
+				else:
+					clipLayerPath += temp04Path+";"
+
+				processing.runalg("qgis:clip", viirs_layer, austral_layer, temp05Path)
+				tempAustral_layer = QgsVectorLayer(temp05Path, "austral", "ogr")
+				if not tempAustral_layer.isValid():
+					print "Austral clip layer error"
+				else:
+					clipLayerPath += temp05Path+";"
+								
+				layerFeaturesCount = 0
+				layerFeaturesCount += tempNorthG_layer.featureCount()
+				layerFeaturesCount += tempNorthC_layer.featureCount()
+				layerFeaturesCount += tempCenter_layer.featureCount()
+				layerFeaturesCount += tempSouth_layer.featureCount()
+				layerFeaturesCount += tempAustral_layer.featureCount()
+
+				if layerFeaturesCount == 0:
+					print "No fire detected"
+				else:
+					clipLayerPath = clipLayerPath.strip(";")
+					finalClipPath = os.path.join(dayfolderPath, "VIIRS_"+dayfolder+"_"+timeupdate+"_CLIP.shp")
+					processing.runalg("qgis:mergevectorlayers", clipLayerPath, finalClipPath)
+					print "VIIRS shape created successfully"
+
+				
+				shutil.rmtree(tempPath)
+				elapsed = timeit.default_timer() - start_time
+				print "time: ", elapsed
 
 
 
--- getFTP.py
+++ getFTP.py
@@ -5,6 +5,12 @@ import os
 import getopt
 import sys
 
+
+#Set user and pass for VIIRS server
+USERNAME = "benjartv"
+PASSWORD = "Ironmaiden1991"
+
+
 def usage():
     print "\n Get VIIRS data from NASA"
     print " -d: directory for save VIIRS data"
@@ -33,21 +39,21 @@ def initConfig():
             sys.exit(2)
     return directory
 
-def searchBox(coor):
-	#Chilean block coordinates
-	bloque = [[-17.5, -22.7, -67.8],
-			[-22.7, -24.4, -66.9],
-			[-24.4, -28.4, -68.2],
-			[-28.4, -36.5, -69.5],
-			[-36.5, -38.9, -70.5],
-			[-38.9, -44.3, -71.5],
-			[-44.3, -51.9, -71.0],
-			[-51.9, -55.1, -66.2]]
+# def searchBox(coor):
+# 	#Chilean block coordinates
+# 	bloque = [[-17.5, -22.7, -67.8],
+# 			[-22.7, -24.4, -66.9],
+# 			[-24.4, -28.4, -68.2],
+# 			[-28.4, -36.5, -69.5],
+# 			[-36.5, -38.9, -70.5],
+# 			[-38.9, -44.3, -71.5],
+# 			[-44.3, -51.9, -71.0],
+# 			[-51.9, -55.1, -66.2]]
 
-	for seg in range(len(bloque)):
-		if coor[0] <= bloque[seg][0] and coor[0] > bloque[seg][1] and coor[1] <= bloque[seg][2]:
-			return True
-	return False
+# 	for seg in range(len(bloque)):
+# 		if coor[0] <= bloque[seg][0] and coor[0] > bloque[seg][1] and coor[1] <= bloque[seg][2]:
+# 			return True
+# 	return False
 
 if __name__ == "__main__":
 	#Set directory for save data
@@ -59,15 +65,16 @@ if __name__ == "__main__":
 	dayfolder = str(year)+str(day_of_year)
 	filename = "VIIRS_I_South_America_VNP14IMGTDL_NRT_"+dayfolder+".txt"
 
-	if not os.path.isdir(directory+"/"+dayfolder):
-		os.makedirs(directory+"/"+dayfolder)
-
-	#Set user and pass for VIIRS server
-	username = "benjartv"
-	password = "Ironmaiden1991"
+	folderPath = os.path.join(directory+"/"+dayfolder)
+	if not os.path.isdir(folderPath):
+		try:
+			os.makedirs(folderPath)
+			print "Directory of the day created successfully"
+		except Exception:
+			print "Error: Could not create directory of the day"
 
 	#Get file from Active Fire Data NASA (via FTP)
-	req = urlopen('ftp://'+username+':'+password+'@nrt3.modaps.eosdis.nasa.gov/FIRMS/viirs/South_America/')
+	req = urlopen('ftp://'+USERNAME+':'+PASSWORD+'@nrt3.modaps.eosdis.nasa.gov/FIRMS/viirs/South_America/')
 	data = req.read()
 
 	if filename not in data:
@@ -78,35 +85,38 @@ if __name__ == "__main__":
 		timeupdate = content[index-1].split(":")
 		timeupdate = timeupdate[0]+timeupdate[1]
 
-		filenamePath = directory+"/"+dayfolder+"/VIIRS_I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate+".txt"
-		fileoutPath = directory+"/"+dayfolder+"/VIIRS_I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate+"_clean.txt"
-		logPath = directory+"/"+dayfolder+"/VIIRS_log.log"
+		filenamePath = os.path.join(folderPath, "VIIRS_I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate+".txt")
+		# fileoutPath = os.path.join(folderPath, "VIIRS_I_South_America_VNP14IMGTDL_NRT_"+dayfolder+"_"+timeupdate+"_clean.txt")
+		logPath = os.path.join(folderPath, "VIIRS_log.log")
 
-		if not os.path.isfile(filenamePath):		
-			urllib.urlretrieve('ftp://'+username+':'+password+'@nrt3.modaps.eosdis.nasa.gov/FIRMS/viirs/South_America/'+filename, filenamePath)
+		if not os.path.isfile(filenamePath):
+			try:
+				urllib.urlretrieve('ftp://'+USERNAME+':'+PASSWORD+'@nrt3.modaps.eosdis.nasa.gov/FIRMS/viirs/South_America/'+filename, filenamePath)
+			except:
+				print "Error downloading the viirs file"
+				sys.exit(1)
+			# #Remove data from other countries than Chile
+			# file = open(filenamePath)
+			# content = file.readlines()
+			# file.close()
 
-			#Remove data from other countries than Chile
-			file = open(filenamePath)
-			content = file.readlines()
-			file.close()
+			# textout = ""
+			# first = content.pop(0)
+			# textout += first
 
-			textout = ""
-			first = content.pop(0)
-			textout += first
+			# for line in content:
+			# 	aux = line.strip().split(",")
+			# 	coordanda = [float(aux[0]), float(aux[1])]
+			# 	if searchBox(coordanda):
+			# 		auxText = ""
+			# 		for i in range(len(aux)-1):
+			# 			auxText += aux[i]+","
+			# 		auxText += aux[-1]+"\n"
+			# 		textout += auxText
 
-			for line in content:
-				aux = line.strip().split(",")
-				coordanda = [float(aux[0]), float(aux[1])]
-				if searchBox(coordanda):
-					auxText = ""
-					for i in range(len(aux)-1):
-						auxText += aux[i]+","
-					auxText += aux[-1]+"\n"
-					textout += auxText
-
-			file = open(fileoutPath,"w")
-			file.write(textout)
-			file.close()
+			# file = open(fileoutPath,"w")
+			# file.write(textout)
+			# file.close()
 
 			logfile = open(logPath, "a")
 			logfile.write(timeupdate+"\n")
